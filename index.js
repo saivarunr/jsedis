8af3fcc5cc6015eb840e0a71fb37bdccaedf9087
Sai Varun Reddy Daram <saivarunvishal@gmail.com>
2018-06-30 00:03:13 +0530

    Publish and Subscribe added

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 class Jsedis{
 	constructor(){
 		this.store={};
+		this.channels={};
 	}
 	append(key,value){
 		if(['string','number'].includes(typeof(value))){
@@ -9,6 +10,26 @@ class Jsedis{
 			return value.length;
 		}
 		console.error(`Type ${typeof(value)} not supported for append`)
+	}
+	subscribe(channel,callback){
+		if(typeof(callback)!="function"){
+			console.error(`Callbacks for channel should be a function, not ${typeof(callback)}`);
+			return false;
+		}
+		if(!this.channels.hasOwnProperty(channel)){
+			this.channels[channel]=new Array();
+		}
+		this.channels[channel].push(callback);
+		return true;
+	}
+	publish(channel,message){
+		if(!this.channels.hasOwnProperty(channel)){
+			return false;
+		}
+		this.channels[channel].forEach(function(callBackFunc){
+			callBackFunc(message);
+		});
+		return true;
 	}
 }
 
